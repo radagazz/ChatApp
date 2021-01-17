91be9b246f75f5de1575b02e70319b150857e3e8
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2021-01-18 02:26:57 +0700

    added IQKeyboardManagerSwift to project

--- ChatApp/Controller/ChatViewController.swift
+++ ChatApp/Controller/ChatViewController.swift
@@ -15,11 +15,7 @@ class ChatViewController : UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
-    var messages : [Message] = [
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "hello"),
-        Message(sender: "1@2.com", body: "how are you")
-    ]
+    var messages : [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +31,9 @@ class ChatViewController : UIViewController {
     func loadMessages(){
         
         
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             
             self.messages = []
             if let e = error {
@@ -63,7 +61,8 @@ class ChatViewController : UIViewController {
         if let messageBody = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField : Date().timeIntervalSince1970
             ]) { (error) in
                 if let e = error {
                     print("There is an issue saving data to firestore \(e).")
